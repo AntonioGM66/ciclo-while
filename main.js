809506dc8f6751cc5c61b2ef5f56130b7f6e26d2
AntonioGM66 <oitog3@gmail.com>
2024-08-27 19:19:33 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,100 @@
+/*let num = prompt("Ingresa un número"), i=1;
+
+while(i<=num)
+{
+    if(i%5===0)
+    {
+        console.log(i);
+    }
+    i++;
+}
+
+let num1 = parseInt(prompt("Ingresa un número entre 1 y 50:")), num2 = parseInt(prompt("Ingresa otro número entre 1 y 50:"));
+
+if ((num1 >= 1 && num1 <= 50) && (num2 >= 1 && num2 <= 50)) 
+{
+    let i = 1;
+    while (i <= 50) 
+    {
+        if (i === numero1 || i === num2) 
+        {
+            console.log(i + " ¡Lotería!");
+        } else 
+        {
+            console.log(i);
+        }
+        i++;
+    }
+} else 
+{
+    console.log("Por favor, ingresa números válidos entre 1 y 50.");
+}
+
+let array = [], num, i=0;
+
+while(true)
+{
+    num = parseInt(prompt("Ingresa un numero e ingresa 0 para terminar"));
+    if (num===0)
+    {
+        break;
+    }
+    else
+    {
+        array.push(num);
+    }
+}
+console.log(array);
+
+let palabra = "", texto;
+
+while (true) 
+{
+    texto = prompt("Ingresa una letra o palabra, para terminar deja vacío y presiona Enter:");
+    if (texto === "") 
+    {
+        break;
+    }
+    palabra += texto + " ";
+}
+console.log("Palabras concatenadas:", palabra);
+
+
+let dia;
+
+while (true) {
+    dia = prompt("Ingresa un día de la semana: ").toLowerCase();
+    switch (dia) 
+    {
+        case "lunes":
+            alert("¡Inicio de la semana, ánimo!");
+            break;
+        case "martes":
+            alert("Martes de productividad, sigue adelante.");
+            break;
+        case "miércoles":
+            alert("Mitad de semana, ¡ya casi lo logras!");
+            break;
+        case "jueves":
+            alert("Jueves de reflexión, ¡prepárate para el fin de semana!");
+            break;
+        case "viernes":
+            alert("¡Viernes al fin! El fin de semana está cerca.");
+            break;
+        case "sábado":
+            alert("Sábado de relajación, disfruta tu día.");
+            break;
+        case "domingo":
+            alert("Ve a descansar");
+            break;
+        default:
+            alert("Por favor, ingresa un día válido.");
+            continue;
+    }
+    if (dia === "domingo") 
+    {
+        break;
+    }
+}*/
+
+
